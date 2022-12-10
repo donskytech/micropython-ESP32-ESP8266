ecf9e25a89fdde11b5804d33ede72ebf0434c118
donskytech <donsky.iot.tech@gmail.com>
2022-12-10 13:11:12 +0800

    Basic DS18b20 MicroPython Code

---
+++ ds18b20/test_ds18b20.py
@@ -0,0 +1,19 @@
+import machine, onewire, ds18x20, time
+ 
+ds_pin = machine.Pin(21)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+ 
+roms = ds_sensor.scan()
+
+if not roms:
+    raise RuntimeError("Found no DS18b20")
+
+for rom in roms:
+    print(ds_sensor.read_temp(rom))
+    
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+  for rom in roms:
+    print(ds_sensor.read_temp(rom))
+  time.sleep(1)
