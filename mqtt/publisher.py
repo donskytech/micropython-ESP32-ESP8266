8a2174b7df9fc46c2178b155546239697547b72f
donskytech <donsky.iot.tech@gmail.com>
2023-01-30 21:09:19 +0800

    Update publish and subscribe example

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -2,20 +2,34 @@ import time
 import ubinascii
 import machine
 from umqtt.simple import MQTTClient
+import random
 
 # Default MQTT server to connect to
 SERVER = "192.168.100.22"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 TOPIC = b"temperature"
 
-
+def reset():
+    print("Resetting...")
+    time.sleep(5)
+    machine.reset()
+    
 def main():
     mqttClient = MQTTClient(CLIENT_ID, SERVER, keepalive=60)
     mqttClient.connect()
     print(f"Connected to MQTT  Broker :: {SERVER}")
 
-    mqttClient.publish(TOPIC, b"49")
+    while True:
+        random_temp = random.randint(20, 50)
+        print(f"Publishing temperature :: {random_temp}")
+        mqttClient.publish(TOPIC, str(random_temp).encode())
+        time.sleep(3)
     mqttClient.disconnect()
     
+    
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except OSError as e:
+        print("Error: " + str(e))
+        reset()
--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -8,11 +8,20 @@ MQTT_BROKER = "192.168.100.22"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 TOPIC = b"temperature"
 
+# Ping the MQTT broker since we are not publishing any message
+last_ping = time.time()
+ping_interval = 60
+
 # Received messages from subscriptions will be delivered to this callback
 def sub_cb(topic, msg):
     print((topic, msg))
 
 
+def reset():
+    print("Resetting...")
+    time.sleep(5)
+    machine.reset()
+    
 def main():
     mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, keepalive=60)
     mqttClient.set_callback(sub_cb)
@@ -20,7 +29,7 @@ def main():
     mqttClient.subscribe(TOPIC)
     print(f"Connected to MQTT  Broker :: {MQTT_BROKER}, and waiting for callback function to be called!")
     while True:
-        if True:
+        if False:
             # Blocking wait for message
             mqttClient.wait_msg()
         else:
@@ -28,10 +37,22 @@ def main():
             mqttClient.check_msg()
             # Then need to sleep to avoid 100% CPU usage (in a real
             # app other useful actions would be performed instead)
+            global last_ping
+#             current_time = time.time()
+            if (time.time() - last_ping) >= ping_interval:
+                mqttClient.ping()
+                last_ping = time.time()
+                now = time.localtime()
+                print(f"Pinging MQTT Broker, last ping :: {now[0]}/{now[1]}/{now[2]} {now[3]}:{now[4]}:{now[5]}")
             time.sleep(1)
-
+            
+    print("Disconnecting...")
     mqttClient.disconnect()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except OSError as e:
+        print("Error: " + str(e))
+        reset()
